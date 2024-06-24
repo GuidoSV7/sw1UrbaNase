750e7a44f7b9b252c40993fdfc8ca0911a104576
Guido Salazar Vargas <Guido.salazar.vargas7@gmail.com>
2024-06-24 02:58:56 -0400

    Deploy 3

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  const logger = new Logger('Bootstrap');
 
   app.setGlobalPrefix('api');
 
@@ -20,13 +22,14 @@ async function bootstrap() {
 
 
    const config = new DocumentBuilder()
-   .setTitle('Software Sig RestFul API')
-   .setDescription('Software Sig Endpoints')
+   .setTitle('Software 1 API')
+   .setDescription('Software 1 Endpoints')
    .setVersion('1.0')
    .build();
  const document = SwaggerModule.createDocument(app, config);
  SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+ await app.listen(process.env.PORT);
+ logger.log(`App running on port ${ process.env.PORT }`);
 }
 bootstrap();
